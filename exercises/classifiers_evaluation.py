470e231ecc1222f02b3c5d32da2dcfe76e4007bd
xsnatchysquidx <51396413+xsnatchysquidx@users.noreply.github.com>
2022-04-27 09:38:30 +0300

    Update classifiers_evaluation.py

--- exercises/classifiers_evaluation.py
+++ exercises/classifiers_evaluation.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 import plotly.io as pio
 from plotly.subplots import make_subplots
 import plotly.express as px
+from math import atan2, pi
 
 pio.templates.default = "simple_white"
 
@@ -64,6 +65,28 @@ def run_perceptron():
             .update_xaxes(title="Iteration") \
             .update_yaxes(title="Misclassification Error")
         fig.show()
+
+
+def get_ellipse(mu: np.ndarray, cov: np.ndarray):
+    """
+    Draw an ellipse centered at given location and according to specified covariance matrix
+    Parameters
+    ----------
+    mu : ndarray of shape (2,)
+        Center of ellipse
+    cov: ndarray of shape (2,2)
+        Covariance of Gaussian
+    Returns
+    -------
+        scatter: A plotly trace object of the ellipse
+    """
+    l1, l2 = tuple(np.linalg.eigvalsh(cov)[::-1])
+    theta = atan2(l1 - cov[0, 0], cov[0, 1]) if cov[0, 1] != 0 else (np.pi / 2 if cov[0, 0] < cov[1, 1] else 0)
+    t = np.linspace(0, 2 * pi, 100)
+    xs = (l1 * np.cos(theta) * np.cos(t)) - (l2 * np.sin(theta) * np.sin(t))
+    ys = (l1 * np.sin(theta) * np.cos(t)) + (l2 * np.cos(theta) * np.sin(t))
+
+    return go.Scatter(x=mu[0] + xs, y=mu[1] + ys, mode="lines", marker_color="black")
 
 
 def compare_gaussian_classifiers():
